fcfc48800cd801029f17deb00cccb65abd03ded0
lamat <lamat032025@gmail.com>
2024-07-15 09:45:18 +0100

    fridst

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = 'django-insecure-cjce5g%-(-h#budivay0ft(rffn+*ab&1olcg&k=yr(0uxul*f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -118,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/' 
-
+STATIC_URL = '/static/' 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS =['static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
